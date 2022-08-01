9fc02b6b0ac7937e0dec0412670d0c9156050021
abusayeed4bd <abusayeed4bd@gmail.com>
2022-08-01 10:45:32 +0600

    socket.io connection and all action api created

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ app.get('/', (req, res) => {
     )
 })
 
+// socket connection
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended');
+
+        socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+            io.on(userToCall).emit('clluser', { signale: signalData, from, name })
+        })
+
+        socket.on('answercall', (data) => {
+            io.to(data.to).emit('callaccepted', data.signal)
+        })
+    })
+})
+
+
+
+
 server.listen(PORT, (req, res) => console.log(`listening on ${PORT}`))
 
 
