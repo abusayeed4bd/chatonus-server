04d375aa63a65479698eb3fa419a9f75b8939773
abusayeed4bd <abusayeed4bd@gmail.com>
2022-08-01 10:35:25 +0600

    primary get api and listenig added

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const express = require('express')();
+const app = require('express')();
 const server = require('http').createServer(app);
 const cors = require('cors');
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send(
+        "welcome to chatonus!"
+    )
+})
+
+server.listen(PORT, (req, res) => console.log(`listening on ${PORT}`))
 
 
